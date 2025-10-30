a0814d2b929f3f38c62a4a5453d63009e2db2170
Hangfan Li <lihangfan@iscas.ac.cn>
2025-10-30 17:44:55 +0800

    A210: Providing envs for bootstd

--- include/configs/a210-evb.h
+++ include/configs/a210-evb.h
@@ -7,4 +7,12 @@
 #ifndef __CONFIG_A210_EVB_H
 #define __CONFIG_A210_EVB_H
 
+#define CFG_EXTRA_ENV_SETTINGS \
+	"kernel_addr_r=0x82000000\0" \
+	"kernel_comp_addr_r=0x8a000000\0" \
+	"kernel_comp_size=0x6000000\0" \
+	"fdt_addr_r=0x9ff00000\0" \
+	"ramdisk_addr_r=0x9e000000\0" \
+	"pxefile_addr_r=0x88000000\0" \
+	"\0"
 #endif /* __CONFIG_A210_EVB_H */
