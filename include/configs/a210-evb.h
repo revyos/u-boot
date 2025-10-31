a9fa62956b42d791759622883d65f7177184196c
Hangfan Li <lihangfan@iscas.ac.cn>
2025-10-31 13:15:05 +0800

    A210: rearrange initrd location

--- include/configs/a210-evb.h
+++ include/configs/a210-evb.h
@@ -12,7 +12,7 @@
 	"kernel_comp_addr_r=0x8a000000\0" \
 	"kernel_comp_size=0x6000000\0" \
 	"fdt_addr_r=0x9ff00000\0" \
-	"ramdisk_addr_r=0x9e000000\0" \
+	"ramdisk_addr_r=0x90000000\0" \
 	"pxefile_addr_r=0x88000000\0" \
 	"\0"
 #endif /* __CONFIG_A210_EVB_H */
